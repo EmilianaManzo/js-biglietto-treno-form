f8c39fc51150ccbbcfcaf576ffdb5dc056cddc90
EmilianaManzo <emiliana.ma96@gmail.com>
2024-02-21 22:01:15 +0100

    [end] javascript

--- javascript/script.js
+++ javascript/script.js
@@ -6,7 +6,9 @@ let standard;
 let over65;
 let price;
 let message = '';
-
+let form;
+let carriage;
+let code;
 
 // dichiaro dati di partenza
 const priceKm = 0.21 
@@ -22,7 +24,7 @@ name = document.querySelector('.name');
 km = document.querySelector('.km');
 età = document.querySelector('.età');
 colHidden = document.querySelector('.colhidden')
-
+form = document.querySelector('form');
 
 
 // bottoni
@@ -32,13 +34,25 @@ const btncancel = document.querySelector('.btncancel');
 
 
 
+
 btngenerate.addEventListener ('click', function(event){
   event.preventDefault
-  price = priceKm * km.value;
+  price = parseInt(priceKm * km.value);
   message = 'Biglietto standard'
   console.log(name.value, km.value, età.value, price);
 
   colHidden.classList.remove('d-none')
+
+  if(name.value === ''){
+    alert('Attenzione! Inserire nome e cognome.')
+  } else if (km.value === ''){
+    alert('Attenzione! Inserire km.')
+
+  }if(età.value === ''){
+    alert('Attenzione! Scegliere range età.')
+  }
+
+  
   
   if(età.value === minorenne){
   price = (price * discountYounger/100)
@@ -49,10 +63,24 @@ btngenerate.addEventListener ('click', function(event){
   message = 'Biglietto over 65'
   
   }
-  document.querySelector('.passenger').innerHTML += name.value 
-  document.querySelector('.offer').innerHTML += message
-  document.querySelector('.cost').innerHTML += price
+
+  carriage=Math.floor(Math.random() * 10) + 1;
+  code=Math.floor(Math.random() * 1000) + 1;
+  console.log(carriage, code);
+
+
+
+  document.querySelector('.passenger').innerHTML += '' + name.value 
+  document.querySelector('.offer').innerHTML += '' + message
+  document.querySelector('.cost').innerHTML += '' + price.toFixed(2) + '&euro;'
+  document.querySelector('.carriage').innerHTML += '' + carriage
+  document.querySelector('.code').innerHTML += '' + code
+
 })
+
+  
+
+
 
 btncancel.addEventListener ('click', function(){
   name.value = '';
